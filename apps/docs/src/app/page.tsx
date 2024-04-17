b1060293b9a150892df9d013cd948fec1b2d508d
cgoing <neo.cgoing@gmail.com>
2024-04-17 17:30:40 +0900

    added : plob gen component

--- apps/docs/src/app/page.tsx
+++ apps/docs/src/app/page.tsx
@@ -1,8 +1,8 @@
-'use client';
-import Image from 'next/image';
-import { Card } from '@repo/react-kit/card';
-import { useEffect } from 'react';
-import { helloWorld } from '@repo/shared';
+"use client";
+import Image from "next/image";
+import { Card } from "@repo/react-kit/card";
+import { useEffect } from "react";
+import { helloWorld } from "@repo/shared";
 
 function Gradient({
   conic,
@@ -16,39 +16,39 @@ function Gradient({
   return (
     <span
       className={`absolute mix-blend-normal will-change-[filter] rounded-[100%] ${
-        small ? 'blur-[32px]' : 'blur-[75px]'
-      } ${conic ? 'bg-glow-conic' : ''} ${className}`}
+        small ? "blur-[32px]" : "blur-[75px]"
+      } ${conic ? "bg-glow-conic" : ""} ${className}`}
     />
   );
 }
 
 const LINKS = [
   {
-    title: 'Docs',
-    href: 'https://turbo.build/repo/docs',
-    description: 'Find in-depth information about Turborepo features and API.',
+    title: "Docs",
+    href: "https://turbo.build/repo/docs",
+    description: "Find in-depth information about Turborepo features and API.",
   },
   {
-    title: 'Learn',
-    href: 'https://turbo.build/repo/docs/handbook',
-    description: 'Learn more about monorepos with our handbook.',
+    title: "Learn",
+    href: "https://turbo.build/repo/docs/handbook",
+    description: "Learn more about monorepos with our handbook.",
   },
   {
-    title: 'Templates',
-    href: 'https://turbo.build/repo/docs/getting-started/from-example',
-    description: 'Choose from over 15 examples and deploy with a single click.',
+    title: "Templates",
+    href: "https://turbo.build/repo/docs/getting-started/from-example",
+    description: "Choose from over 15 examples and deploy with a single click.",
   },
   {
-    title: 'Deploy',
-    href: 'https://vercel.com/new',
+    title: "Deploy",
+    href: "https://vercel.com/new",
     description:
-      'Instantly deploy your Turborepo to a shareable URL with Vercel.',
+      "Instantly deploy your Turborepo to a shareable URL with Vercel.",
   },
 ];
 
 export default function Page() {
   useEffect(() => {
-    helloWorld('ok~');
+    helloWorld("ok~");
   }, []);
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
@@ -65,7 +65,7 @@ export default function Page() {
             rel="noopener noreferrer"
             target="_blank"
           >
-            By{' '}
+            By{" "}
             <Image
               alt="Vercel Logo"
               className="invert"
---
+++ packages/react-kit/src/components/button.tsx
@@ -0,0 +1,7 @@
+export default function Button() {
+  return (
+    <div>
+      <h1>Button Component</h1>
+    </div>
+  );
+}
