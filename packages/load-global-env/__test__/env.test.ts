53c904e1fdab6c34137f2646cb52eb2c8a9d1ed9
cgoing <neo.cgoing@gmail.com>
2024-05-04 22:18:28 +0900

    added worksapce load-global-env

---
+++ packages/load-global-env/__test__/env.test.ts
@@ -0,0 +1,29 @@
+import { expect, test } from 'vitest';
+import { join } from 'node:path';
+import { writeFileSync, rmSync } from 'node:fs';
+
+test('load-env', async () => {
+  const MOCK_ENV_KEY = 'CGOING_EMAIL';
+  const MOCK_ENV_VALUE = 'neo.cgoing@gmail.com';
+
+  expect(process.env[MOCK_ENV_KEY]).toBeFalsy();
+
+  const ID = `${Date.now()}-TEST`;
+
+  // set NODE_ENV for test
+  process.env.NODE_ENV = ID;
+
+  const filePath = join(process.cwd(), 'env', `.env.${ID}`);
+
+  // write test env file
+  writeFileSync(filePath, `${MOCK_ENV_KEY}=${MOCK_ENV_VALUE}`);
+
+  // excute loader
+  await import('../loader');
+
+  expect(process.env[MOCK_ENV_KEY]).toBe(MOCK_ENV_VALUE);
+
+  rmSync(filePath, {
+    force: true,
+  });
+});
---
+++ packages/load-global-env/loader.ts
@@ -0,0 +1,21 @@
+import { config } from 'dotenv';
+import { expand } from 'dotenv-expand';
+import { existsSync } from 'node:fs';
+import { join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const ROOT = join(fileURLToPath(import.meta.url), '../', 'env');
+
+const localEnv = `${ROOT}/.env.local`;
+
+existsSync(localEnv) && expand(config({ path: localEnv }));
+
+const envPath = `${ROOT}/.env.${process.env.NODE_ENV}`;
+
+existsSync(envPath) && expand(config({ path: envPath }));
+
+expand(
+  config({
+    path: `${ROOT}/.env`,
+  }),
+);
