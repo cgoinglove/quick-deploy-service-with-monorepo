5c47825b68cfc4ceda55d558a9ab46c468248b7d
cgoing <neo.cgoing@gmail.com>
2024-05-15 17:12:59 +0900

    next.config 수정

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  transpilePackages: ["@repo/react-kit", "@repo/database-service"],
+  transpilePackages: ["@repo/react-kit"],
   experimental: {
     serverComponentsExternalPackages: ["typeorm"],
   },
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  transpilePackages: ['@repo/react-kit'],
+  transpilePackages: ["@repo/react-kit"],
+  experimental: {
+    serverComponentsExternalPackages: ["typeorm"],
+  },
 };
