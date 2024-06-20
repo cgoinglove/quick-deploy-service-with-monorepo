cb30251dd760a017d27bda9ac9b61f075c19e2c0
cgoing <neo.cgoing@gmail.com>
2024-06-20 14:48:46 +0900

    @repo/service added peerDependencies

--- packages/config-tailwind/tailwind.config.ts
+++ packages/config-tailwind/tailwind.config.ts
@@ -1,19 +1,19 @@
-import type { Config } from 'tailwindcss';
-import { join } from 'node:path';
-import { fileURLToPath } from 'node:url';
+import type { Config } from "tailwindcss";
+import { join } from "node:path";
+import { fileURLToPath } from "node:url";
 
 export const REACT_KIT_CONTENT_PATH = join(
   fileURLToPath(import.meta.url),
-  '../../../packages/react-kit/src/components/**/*.tsx',
+  "../../../packages/react-kit/src/components/**/*.tsx",
 );
 
 // We want each package to be responsible for its own content.
-const config: Omit<Config, 'content'> = {
+const config: Omit<Config, "content"> = {
   theme: {
     extend: {
       backgroundImage: {
-        'glow-conic':
-          'conic-gradient(from 180deg at 50% 50%, #2a8af6 0deg, #a853ba 180deg, #e92a67 360deg)',
+        "glow-conic":
+          "conic-gradient(from 180deg at 50% 50%, #2a8af6 0deg, #a853ba 180deg, #e92a67 360deg)",
       },
     },
   },
